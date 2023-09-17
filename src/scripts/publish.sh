e578d76002c932363396994d1f151f5fee6dd8dc
Carlos Fdez. Llamas <hello@sirikon.me>
2023-09-17 22:15:56 +0200

    Start "publish" script

---
+++ src/scripts/publish.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+function main {
+  version="$(sed 's/[ \n]//g' <VERSION)"
+  commit="$(git rev-parse --verify HEAD)"
+
+  log "Publishing Cow" \
+    "  version: ${version}" \
+    "   commit: ${commit}"
+}
+
+function log { (
+  printf "\e[2m▓▒░ %s\e[m\n" "${@}"
+); }
+
+main
