938ccce5735bff931ddfc80adb7c4f39948ea49c
Carlos Fdez. Llamas <hello@sirikon.me>
2023-09-30 13:16:27 +0200

    Publish script

--- src/scripts/publish.sh
+++ src/scripts/publish.sh
@@ -1,13 +1,28 @@
 #!/usr/bin/env bash
 set -euo pipefail
+cd "$(realpath "$(dirname "${BASH_SOURCE[0]}")/../..")"
 
 function main {
-  version="$(sed 's/[ \n]//g' <VERSION)"
-  commit="$(git rev-parse --verify HEAD)"
+  COW_VERSION="$(sed 's/[ \n]//g' <VERSION)"
+  COW_COMMIT="$(git rev-parse --verify HEAD)"
+  export COW_VERSION
+  export COW_COMMIT
+
+  export COW_IMAGE="sirikon/cow:${COW_VERSION}-${COW_COMMIT}"
 
   log "Publishing Cow" \
-    "  version: ${version}" \
-    "   commit: ${commit}"
+    "  version: ${COW_VERSION}" \
+    "   commit: ${COW_COMMIT}"
+
+  log "Building"
+  docker compose \
+    -f ./docker-compose.yml \
+    -f ./src/docker-environment/docker-compose.base.yml \
+    -f ./src/docker-environment/docker-compose.build.yml \
+    build
+  
+  log "Pushing"
+  docker push "${COW_IMAGE}"
 }
 
 function log { (
