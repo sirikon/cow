07cf96e028eb3d01659af2d9ceed09e853389e98
Carlos Fdez. Llamas <hello@sirikon.me>
2023-11-21 19:10:48 +0100

    remove unused method

--- src/cow/src/cow/paths.py
+++ src/cow/src/cow/paths.py
@@ -4,6 +4,3 @@ from os.path import join
 class PathsProvider:
     def get_config_path(self) -> str:
         return "/config/config.toml"
-
-    def get_project_base_path(self, project_name: str) -> str:
-        return join("/projects", project_name)
